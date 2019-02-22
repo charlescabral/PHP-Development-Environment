7738838651554848b47c009f5c18ad9761ccde43
Charles Cabral <charles.cabral.neto@gmail.com>
2019-02-22 18:10:29 -0300

    update structure

--- www/index.php
+++
@@ -1,4 +0,0 @@
-<?php 
-    echo "<pre><h2>Criando um ambiente de desenvolvimento PHP com Docker Compose.</h2></pre><br/>";
-    mysqli_connect("db", "root", "phprs") or die(mysqli_error()); 
-    echo "<pre><h3>Connected to MySQL<h3></pre><br/>";
